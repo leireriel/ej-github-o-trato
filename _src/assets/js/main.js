75df436a8a6400313710e16f327c7c9d661c3bb6
leireriel <leirericop@gmail.com>
2019-04-14 14:37:42 +0200

    create fetch and extract letters

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('>> Ready :)');
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const list = document.querySelector('.list');
+
+function findName() {
+    fetch(`https://api.github.com/users/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            let completeName = data.name;
+            console.log(completeName);
+            for (let firstName of completeName) {
+                firstName = firstName;
+                console.log(firstName);
+            }
+            list.innerHTML = '<li>firstName</li>';
+        });
+}
+
+
+//Function that allows to press Enter besides search button
+function enterKey(e) {
+    if (e.keycode == 13) {
+        findName();
+    }
+}
+
+btn.addEventListener('click', findName);
+window.addEventListener('keydown', enterKey);
