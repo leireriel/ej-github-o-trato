644d67155ec065365f8911531ef673d6970eb1ca
leireriel <leirericop@gmail.com>
2019-04-14 19:02:11 +0200

    finish js :)

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,24 +4,53 @@ const input = document.querySelector('.input');
 const btn = document.querySelector('.btn');
 const list = document.querySelector('.list');
 
-function findName() {
-    fetch(`https://api.github.com/users/${input.value}`)
+const name = JSON.parse(localStorage.getItem('name'));
+
+//Function to save the data of my username in LS
+function saveLeireInLS() {
+    fetch(`https://api.github.com/users/leireriel`)
         .then(response => response.json())
         .then(data => {
-            let completeName = data.name;
-            console.log(completeName);
-            for (let firstName of completeName) {
-                firstName = firstName;
-                console.log(firstName);
-            }
-            list.innerHTML = '<li>firstName</li>';
+            const leireName = data.name;
+            localStorage.setItem('name', JSON.stringify(leireName));
+            name = JSON.parse(localStorage.getItem('name'));
         });
+}
+
+//Function to print name on screen
+function printName(username) {
+    console.log(username);
+    list.innerHTML = '';
+    for (let i = 0; i < username.length; i++) {
+        if (username[i] === ' ') {
+            break;
+        }
+        let letter = username[i];
+        list.innerHTML += `<li>${letter}</li>`;
+    }
+}
+
+//Function to find user name
+function findName() {
+    if (input.value === 'leireriel') {
+        if (name === null) {
+            saveLeireInLS();
+        }
+        printName(name);
+    } else {
+        fetch(`https://api.github.com/users/${input.value}`)
+            .then(response => response.json())
+            .then(data => {
+                let completeName = data.name;
+                printName(completeName);
+            });
+    }
 }
 
 
 //Function that allows to press Enter besides search button
 function enterKey(e) {
-    if (e.keycode == 13) {
+    if (e.keyCode == 13) {
         findName();
     }
 }
